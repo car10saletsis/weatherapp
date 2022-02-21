fe000f9a9cecd51e1b655e9d1c8a697668283ff7
car10saletsis <car10s@live.com.mx>
2022-02-21 01:07:54 -0600

    Intenta replicar lo de la primera clase, falla en dataclass

--- app/src/main/java/com/example/weatherapp/network/Main.kt
+++ app/src/main/java/com/example/weatherapp/network/Main.kt
@@ -1,8 +1,3 @@
 package com.example.weatherapp.network
 
-data class Main (val temp: Double,
-                 val feels_like: Double,
-                 val temp_min: Double,
-                 val temp_max: Double,
-                 val pressure: Int,
-                 val humidity: Int)
+data class Main()
